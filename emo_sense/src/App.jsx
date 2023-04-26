dadab18d42e04ab1db630eb899f37737339ebc9f
csakafeda <bako.feodora97@gmail.com>
2023-04-26 21:58:03 +0200

    make packages for pages and components

--- emo_sense/src/App.jsx
+++ emo_sense/src/App.jsx
@@ -1,10 +1,14 @@
+import { Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <Router>
+        <Route path='/'/>
+      </Router>
+    </div>
   )
 }
 
