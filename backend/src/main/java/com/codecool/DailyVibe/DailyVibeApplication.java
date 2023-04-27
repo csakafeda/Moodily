b61c20a647d4983264c2d18a32fa4c7e2fa8e8e2
csakafeda <bako.feodora97@gmail.com>
2023-04-27 20:19:13 +0200

    add basics of backend

---
+++ backend/src/main/java/com/codecool/DailyVibe/DailyVibeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.DailyVibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyVibeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codecool/DailyVibe/DailyVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.DailyVibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
