55525a284cc1490d0af1357c83543da9cdcca9d4
csakafeda <bako.feodora97@gmail.com>
2023-04-28 23:08:02 +0200

    fix packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import HomePage from "../pages/HomePage";
-import ProfilPage from "../pages/ProfilPage";
-import Layout from "../components/Layout";
+import HomePage from "./pages/HomePage";
+import ProfilPage from "./pages/ProfilPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
