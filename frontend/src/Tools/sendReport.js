afb8616c0fce5cb3f8098c9ed01c4c707a48a91a
csakafeda <bako.feodora97@gmail.com>
2023-04-29 15:46:28 +0200

    add post fetch for daily mood

---
+++ frontend/src/Tools/sendReport.js
@@ -0,0 +1,24 @@
+export const submitReport = (rate, text, music, picture) => {
+    console.log({
+            "moodRate": rate,
+            "moodDescription": text,
+            "moodPicture": picture,
+            "moodMusic": music
+        }
+    )
+    fetch("/api", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "moodRate": rate,
+            "moodDescription": text,
+            "moodPicture": music,
+            "moodMusic": picture
+        })
+    })
+        .then(res => {
+            console.log(res.json());
+        })
+}
--- frontend/src/pages/MoodForm.jsx
+++ frontend/src/pages/MoodForm.jsx
@@ -5,50 +5,101 @@ import {
     InputLabel,
     TextField,
     Button,
+    Box
 } from "@mui/material";
-import { useState } from "react";
+import {useState} from "react";
+import {submitReport} from "../Tools/sendReport.js";
 
 export default function MoodForm() {
-    const [moodText, setMoodText] = useState("");
-    const [moodNumber, setMoodNumber] = useState("5");
+    const [text, setText] = useState("");
+    const [rate, setRate] = useState("");
+    const [music, setMusic] = useState("");
+    const [picture, setPicture] = useState("");
 
-    const handleMoodTextChange = (e) => {
-        setMoodText(e.target.value);
+    const handleRateChange = (e) => {
+        setRate(e.target.value);
     };
 
-    const handleMoodNumberChange = (e) => {
-        setMoodNumber(e.target.value);
+    const handleTextChange = (e) => {
+        setText(e.target.value);
     };
 
-    const handleSubmit = () => {};
+    const handleMusicChange = (e) => {
+        setMusic(e.target.value);
+    };
+
+    const handlePictureChange = (e) => {
+        setPicture(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const entries = [...formData.entries()];
+        const formObject = entries.reduce((prev, entry) => {
+            const [key, value] = entry;
+            prev[key] = value;
+            return prev;
+        }, {});
+        console.log({
+            "moodRate": rate,
+            "moodDescription": text,
+            "moodPicture": picture,
+            "moodMusic": music
+        });
+        submitReport(rate, text, music, picture);
+    };
 
     return (
         <>
-            <Container align="center" sx={{ padding: "1rem" }}>
-                <InputLabel id="mood-number">How are you feel today?</InputLabel>
-                <Rating
-                    labelid="mood-number"
-                    defaultValue={5}
-                    max={10}
-                    onChange={handleMoodNumberChange}
-                />
-            </Container>
+            <Box onSubmit={handleSubmit} component={"form"}>
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <InputLabel id="rate">How are you feel today?</InputLabel>
+                    <Rating
+                        labelid="rate"
+                        defaultValue={5}
+                        max={10}
+                        onChange={handleRateChange}
+                    />
+                </Container>
 
-            <Container align="center" sx={{ padding: "1rem" }}>
-                <FormControl sx={{ m: 1, width: 300 }}>
-                    <TextField
-                        label="What happened with you today?"
-                        id="mood-text"
-                        value={moodText}
-                        onChange={handleMoodTextChange}
-                    ></TextField>
-                </FormControl>
-            </Container>
-            <Container align="center" sx={{ padding: "1rem" }}>
-                <Button align="center" onClick={handleSubmit}>
-                    Send
-                </Button>
-            </Container>
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <FormControl sx={{m: 1, width: 300}}>
+                        <TextField
+                            label="What happened with you today?"
+                            id="text"
+                            value={text}
+                            onChange={handleTextChange}
+                        ></TextField>
+                    </FormControl>
+                </Container>
+
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <FormControl sx={{m: 1, width: 300}}>
+                        <TextField
+                            label="Which music represent your day?"
+                            id="music"
+                            onChange={handleMusicChange}
+                        ></TextField>
+                    </FormControl>
+                </Container>
+
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <FormControl sx={{m: 1, width: 300}}>
+                        <TextField
+                            label="Add picture?"
+                            id="picture"
+                            onChange={handlePictureChange}
+                        ></TextField>
+                    </FormControl>
+                </Container>
+
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <Button align="center" type={"submit"}>
+                        Send
+                    </Button>
+                </Container>
+            </Box>
         </>
     );
 }
