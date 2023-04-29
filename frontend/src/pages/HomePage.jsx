f832f842bfe87c27a5dcad1c4b6cfc1216c98d01
csakafeda <bako.feodora97@gmail.com>
2023-04-29 16:48:07 +0200

    add picture, structure homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,24 +1,42 @@
 import {
-    Button, Container
+    Box,
+    Button, Container, Typography
 } from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import catPic from "../public/ca.png";
 
 export default function HomePage() {
     const navigate = useNavigate();
 
     return (
         <>
-            <Container align="center" sx={{padding: "1rem"}}>
-                Welcome to Moodily,
 
-                This website is designed to help you better understand your emotions and improve your well-being.
-                Our website allows you to easily record and track your daily experiences, giving you valuable insights
-                into your emotional state and helping you live in the present moment.
-                <br/>
-                Our goal is to help people become more aware of their feelings and develop a deeper understanding
-                of themselves.
-                We believe that by tracking and reflecting on your emotions, you can improve your overall mental health
-                and happiness.
+            <Container  sx={{padding: "1rem", display: "flex"}}>
+                <Box component="img"
+                     sx={{width: "50%"}}
+                     alt="Logo."
+                     src={catPic}
+                />
+                <Container>
+                    <Typography sx={{padding: "1rem"}}>
+                        Welcome to Moodily,
+                    </Typography>
+                    <Typography>
+                        This website is designed to help you better understand your emotions and improve your
+                        well-being.
+                        Our website allows you to easily record and track your daily experiences, giving you valuable
+                        insights
+                        into your emotional state and helping you live in the present moment.
+                    </Typography>
+                    <Typography>
+                        Our goal is to help people become more aware of their feelings and develop a deeper
+                        understanding
+                        of themselves.
+                        We believe that by tracking and reflecting on your emotions, you can improve your overall mental
+                        health
+                        and happiness.
+                    </Typography>
+                </Container>
             </Container>
             <Container align="center" sx={{padding: "1rem"}}>
                 <Button align="center"
@@ -29,5 +47,6 @@ export default function HomePage() {
                 </Button>
             </Container>
         </>
-    );
+    )
+        ;
 }
