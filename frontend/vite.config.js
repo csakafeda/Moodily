ef0182584534a2d28d8b4c756e677f66e10ba9de
csakafeda <bako.feodora97@gmail.com>
2023-04-29 15:45:49 +0200

    add api config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
