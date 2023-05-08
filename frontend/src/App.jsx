134454e7c1d259b17eb8abc7a00c541c168c896f
csakafeda <bako.feodora97@gmail.com>
2023-05-08 14:32:30 +0200

    add analytic route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./Components/NavBar.jsx";
 import ProfilePage from "./Pages/ProfilePage.jsx";
 import CreatePost from "./Pages/Post/CreatePost.jsx";
 import UpdatePost from "./Pages/Post/UpdatePost.jsx";
+import AnalyticsPage from "./Pages/Analytics/AnalyticPage.jsx";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                         <Route path="/createPost" element={<CreatePost/>}/>
                         <Route path="/updatePost" element={<UpdatePost/>}/>
                         <Route path="/profile" element={<ProfilePage/>}/>
+                        <Route path="/analytics" element={<AnalyticsPage/>}/>
                     </Route>
                 </Routes>
             </Router>
