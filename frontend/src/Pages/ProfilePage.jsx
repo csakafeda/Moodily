21c346771361cba37768fed84f7ad328759d2e1b
csakafeda <bako.feodora97@gmail.com>
2023-05-08 13:54:33 +0200

    add profile page cards

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -1,22 +1,52 @@
-import {Container} from "@mui/material";
-import {useState} from "react";
+import {Container, Card, Box, CardContent, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
 import Loading from "../Components/Loading.jsx";
+import {getAllPost} from "../API/getPosts.js";
 
 export default function ProfilePage() {
     const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        setLoading(true);
+        getAllPost()
+            .then((res) => setData(res))
+            .finally(setLoading(false))
+    }, []);
+
+    if (loading) {
+        return <Loading/>;
+    }
 
     return (
         <>
-            {loading ?
-                (
-                    <Loading/>
-                ) : (
-                    <>
-                        <Container align="center" sx={{padding: "1rem"}}>
-                            <h1>Profile page</h1>
-                        </Container>
-                    </>)
+            {!data
+                ?
+                <div> You have no posts yet.</div>
+                :
+                <Container align="center" sx={{padding: "1rem"}}>
+                    <h1>Profile page</h1>
+                    {data.map((mood) => (
+                        <Card sx={{display: 'flex'}} key={mood.id}>
+                            <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                                <CardContent sx={{flex: '1 1 auto'}}>
+                                    <Typography component="div" variant="h5">
+                                        {mood.moodDescription}
+                                    </Typography>
+                                    <Typography component="div" variant="subtitle1">
+                                        {mood.moodRate}
+                                    </Typography>
+                                    <Typography component="div" variant="subtitle1">
+                                        {mood.moodMusic}
+                                    </Typography>
+                                    <Typography component="div" variant="subtitle1">
+                                        {mood.moodPicture}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    ))}
+                </Container>
             }
         </>
     );
