92740346dd926c1601dc188cdde102e7d298dd71
csakafeda <bako.feodora97@gmail.com>
2023-06-09 23:17:09 +0200

    add test for getAllMoods

--- backend/src/test/java/com/codecool/Moodily/service/MoodServiceTest.java
+++ backend/src/test/java/com/codecool/Moodily/service/MoodServiceTest.java
@@ -1,11 +1,18 @@
 package com.codecool.Moodily.service;
 
+import com.codecool.Moodily.database.models.Mood;
 import com.codecool.Moodily.database.repository.MoodRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class MoodServiceTest {
 
@@ -22,7 +29,13 @@ class MoodServiceTest {
 
     @Test
     void testGetAllMoods() {
+        List<Mood> expectedMoods = Arrays.asList(new Mood(), new Mood(), new Mood());
+        when(moodRepository.findAll()).thenReturn(expectedMoods);
 
+        List<Mood> actualMoods = moodService.getAllMoods();
+
+        assertEquals(expectedMoods, actualMoods);
+        verify(moodRepository, times(1)).findAll();
     }
 
 }
