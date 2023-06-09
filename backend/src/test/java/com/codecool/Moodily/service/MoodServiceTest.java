d4b5c7c49b9f312783d31cb3c5c6e2a2c299caab
csakafeda <bako.feodora97@gmail.com>
2023-06-09 23:18:05 +0200

    add unit test for saveMood

--- backend/src/test/java/com/codecool/Moodily/service/MoodServiceTest.java
+++ backend/src/test/java/com/codecool/Moodily/service/MoodServiceTest.java
@@ -1,6 +1,8 @@
 package com.codecool.Moodily.service;
 
 import com.codecool.Moodily.database.models.Mood;
+import com.codecool.Moodily.database.models.dto.MoodRequestDTO;
+import com.codecool.Moodily.database.models.UserEntity;
 import com.codecool.Moodily.database.repository.MoodRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +10,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -37,5 +41,17 @@ class MoodServiceTest {
         assertEquals(expectedMoods, actualMoods);
         verify(moodRepository, times(1)).findAll();
     }
+
+    @Test
+    void testSaveMood() {
+        MoodRequestDTO moodRequestDTO = new MoodRequestDTO(5, "Test description", "picture.jpg", "music.mp3", LocalDate.now(), LocalDate.now(), new UserEntity());
+        when(moodRepository.save(any(Mood.class))).thenReturn(new Mood());
+
+        Mood savedMood = moodService.saveMood(moodRequestDTO);
+
+        assertNotNull(savedMood);
+        verify(moodRepository, times(1)).save(any(Mood.class));
+    }
+
 
 }
