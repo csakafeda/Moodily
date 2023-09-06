03295cd0ba13b6ce46ca1b3541cfd7b622511fe2
csakafeda <bako.feodora97@gmail.com>
2023-09-06 13:36:22 +0200

    add basic calendar

--- frontend/src/Pages/Calendar/Calendar.jsx
+++ frontend/src/Pages/Calendar/Calendar.jsx
@@ -1,12 +1,27 @@
-import {Typography} from '@mui/material';
+import {Typography} from "@mui/material";
+import Calendar from "react-calendar";
+import {useState} from "react";
+import "./calendar.css";
+import 'react-calendar/dist/Calendar.css';
 
 const CalendarPage = () => {
-    return (
-        <div>
-            <Typography variant="h4">
-                Coming soon! (Calendar)
-            </Typography>
+    const [date, setDate] = useState(new Date());
 
+    const dateChange = (date) => {
+        setDate(date);
+    };
+
+    return (
+        <div className="calendar-container">
+            <Typography className="calendar-title" variant="h4">
+                Your Calendar
+            </Typography>
+            <div className="calendar-wrapper">
+                <Calendar onChange={dateChange} value={date}/>
+            </div>
+            <Typography className="selected-date" variant="subtitle1">
+                {date.toString()}
+            </Typography>
         </div>
     );
 };
