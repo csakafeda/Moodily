84b7811810bd59762feaa9b3e92414c880aff33f
csakafeda <bako.feodora97@gmail.com>
2023-09-06 22:57:40 +0200

    add calendar displays selected day's post

--- frontend/src/Pages/Calendar/Calendar.jsx
+++ frontend/src/Pages/Calendar/Calendar.jsx
@@ -7,19 +7,37 @@ import {getPostDates} from "../../API/userAPI.js";
 import {getUserId} from "../../Tools/userTools.js";
 import "./Calendar.css"
 import Loading from "../../Components/Loading.jsx";
+import {getPostByDateAndUserId} from "../../API/postAPI.js";
+import {MoodCard} from "../../Components/MoodCard.jsx";
 
 const Calendar = () => {
     const [value, setValue] = useState(new Date());
     const [loading, setLoading] = useState(true);
     const [dates, setDates] = useState([]);
+    const [mood, setMood] = useState(null);
 
     useEffect(() => {
         setLoading(true);
         getPostDates(getUserId())
-            .then((res) => setDates(res))
+            .then((res) => setDates(res.sort()))
             .finally(() => setLoading(false));
     }, []);
 
+    useEffect(() => {
+        setLoading(true);
+        console.log(value.toISOString().substring(0, 10))
+        getPostByDateAndUserId(value.toISOString().substring(0, 10), getUserId())
+            .then(res => res == null ? null : setMood(res))
+            .finally(() => setLoading(false));
+    }, [value]);
+
+    const handleChange = (date) => {
+        setValue(new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            date.getDate() + 1
+        ));
+    }
     const tileDisabled = ({date}) => date > new Date();
 
     if (loading) return <Loading/>
@@ -28,20 +46,25 @@ const Calendar = () => {
         <div className="calendar-container">
             <Typography className="">Your Calendar</Typography>
             <CalendarR
-                onChange={(v) => setValue(v)}
-                value={value}
+                onChange={e => handleChange(e)}
                 tileClassName={({date}) => {
-                    let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-                    let month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
-                    const realDate = date.getFullYear() + '-' + month + '-' + day;
-
-                    if (dates.find(val => val.toString() === realDate)) {
+                    const dateWithoutTime = new Date(
+                        date.getFullYear(),
+                        date.getMonth(),
+                        date.getDate() + 1
+                    ).toISOString().split('T')[0];
+                    if (dates.includes(dateWithoutTime)) {
                         return "highlight";
                     }
-
                 }}
                 tileDisabled={tileDisabled}
             />
+            <div>Selected date: {value.toISOString().split('T')[0]}</div>
+
+            {mood && mood.created === value.toISOString().split('T')[0]
+                ? <MoodCard mood={mood}/>
+                : <p> No mood posted</p>}
+
         </div>
     )
 };
