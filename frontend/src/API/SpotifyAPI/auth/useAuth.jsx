764359d533379de4723d8a58626a914abd73668d
csakafeda <bako.feodora97@gmail.com>
2023-09-13 13:04:20 +0200

    add store access_token in localstorage

--- frontend/src/API/SpotifyAPI/auth/useAuth.jsx
+++ frontend/src/API/SpotifyAPI/auth/useAuth.jsx
@@ -7,35 +7,50 @@ export default function useAuth(code) {
     const [expiresIn, setExpiresIn] = useState();
 
     useEffect(() => {
-        axios.post("http://localhost:3001/login", {
-            code,
-        }).then(res => {
-            setAccessToken(res.data.accessToken)
-            setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
+        if (localStorage.getItem("access_token") !== null) {
+            setAccessToken(localStorage.getItem("access_token"));
+        } else {
+            axios.post("http://localhost:3001/login", {
+                code,
+            })
+                .then(res => {
+                    setAccessToken(res.data.accessToken)
+                    setRefreshToken(res.data.refreshToken)
+                    setExpiresIn(res.data.expiresIn)
+                    localStorage.setItem("access_token_code", code);
 
-            window.history.pushState({}, null, "/");
-        }).catch(() => {
-            window.location = "/music"
-        })
-    }, [code]);
+                    window.history.pushState({}, null, "/");
+                })
+                .catch((e) => {
+                    console.error(e);
+                    //TODO display to the user
+                })
+        }
+
+    }, [code])
 
     useEffect(() => {
-        if (!refreshToken || !expiresIn) return;
-        const timeout = setInterval(() => {
+        const storedAccessToken = localStorage.getItem("access_token");
+        const storedExpiresIn = localStorage.getItem("expires_in");
+
+        if (storedAccessToken && storedExpiresIn && Date.now() < parseInt(storedExpiresIn, 10)) {
+            setAccessToken(storedAccessToken);
+        } else if (refreshToken) {
             axios.post("http://localhost:3001/refresh", {
                 refreshToken,
-            }).then(res => {
-                setRefreshToken(res.data.refreshToken)
-                setExpiresIn(res.data.expiresIn)
-            }).catch(() => {
-                window.location = "/music"
             })
-        }, expiresIn - 60 * 1000);
-
-        return () => clearInterval(timeout);
+                .then(res => {
+                    setAccessToken(res.data.accessToken);
+                    setExpiresIn(res.data.expiresIn);
+                    localStorage.setItem("access_token", res.data.accessToken);
+                    localStorage.setItem("expires_in", res.data.expiresIn);
+                })
+                .catch((e) => {
+                    console.error(e);
+                    //TODO display to the user
+                });
+        }
     }, [refreshToken, expiresIn]);
 
     return accessToken
-
 }
