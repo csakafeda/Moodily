3954ddc3f5c4660d5c3087750c7c25a5f89983ba
csakafeda <bako.feodora97@gmail.com>
2023-09-13 13:06:06 +0200

    add iframe with a song list

--- frontend/src/Pages/MusicList/MusicList.jsx
+++ frontend/src/Pages/MusicList/MusicList.jsx
@@ -1,12 +1,23 @@
-import {Typography} from '@mui/material';
 import {SpotifyPage} from "../../API/SpotifyAPI/SpotifyPage.jsx";
 
 const MusicPage = () => {
     return (
         <div>
-            <Typography variant="h4">
-                <SpotifyPage/>
-            </Typography>
+            <h2> Search a music and listen it here</h2>
+            <SpotifyPage/>
+
+            <h2>Listen our list</h2>
+            <iframe
+                style={{
+                    borderRadius: "12px",
+                }}
+                src="https://open.spotify.com/embed/playlist/0P2Tc6l5UFmBsIGv8GxfaS?utm_source=generator"
+                width="100%"
+                height="500px"
+                allowFullScreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            ></iframe>
 
         </div>
     );
