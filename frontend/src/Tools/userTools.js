cf41106f1b734ec66dd44cb5b3e6572346b9a356
csakafeda <bako.feodora97@gmail.com>
2023-09-13 13:01:54 +0200

    remove access_token if log out

--- frontend/src/Tools/userTools.js
+++ frontend/src/Tools/userTools.js
@@ -2,6 +2,7 @@ export const isUserSignedIn = () => localStorage.getItem("userId") !== null;
 export const signUserOut = () => {
     localStorage.removeItem("userId");
     localStorage.removeItem("username");
+    localStorage.removeItem("access_token")
 };
 export const setUserId = (userId) => localStorage.setItem("userId", JSON.stringify(userId));
 export const setUserName = (username) => localStorage.setItem("username", username);
